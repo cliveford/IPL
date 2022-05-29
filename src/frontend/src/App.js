fb6de97216617dc7ebdc46fe77079c645b9b953e
cliveford <cliveford2020@gmail.com>
2022-05-29 17:52:32 +0100

    Match page routing set up

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import { TeamPage } from './pages/TeamPage';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { TeamPage } from "./pages/TeamPage";
+import { MatchPage } from "./pages/MatchPage";
 
 function App() {
   return (
     <div className="App">
-     <TeamPage />
+      <Router>
+        <Routes>
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          ></Route>
+          <Route path="/teams/:teamName" element={<TeamPage />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
